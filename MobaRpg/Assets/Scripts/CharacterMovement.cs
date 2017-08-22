c0584d5459ab5742f1f0cd0a50bee6c5e9b5045e
bernardolv <contact@bernardolimon.com>
2017-08-22 12:10:58 -0500

    Nuevos Scenes
    El monito se mueve. Sigue animar y  luego armar collisions.

---
+++ MobaRpg/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+	Vector3 pos;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.A) && transform.position == pos) {        // Left
+			pos += Vector3.left;
+		}
+		if(Input.GetKey(KeyCode.D) && transform.position == pos) {        // Right
+			pos += Vector3.right;
+		}
+		if(Input.GetKey(KeyCode.W) && transform.position == pos) {        // Up
+			pos += Vector3.up;
+		}
+		if(Input.GetKey(KeyCode.S) && transform.position == pos) {        // Down
+			pos += Vector3.down;
+		}
+		transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);   
+	}
+}
